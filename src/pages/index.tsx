5f0b90240d83739313bfe09c3262bc645daad37a
honjo hiorki <hrkhnj4eng@gmail.com>
2018-04-15 05:53:13 +0900

    try add article

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,57 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import withRedux from '../utils/withRedux'
+import { initStore, addArticle } from '../store'
 import Layout from '../components/Layout'
 import { Link } from '../routes'
 
+
 class Index extends React.Component {
-    public static async getInitialProps() {
-        const isServer = typeof window === 'undefined'
+    public static async getInitialProps({store, isServer, initialState, addArticle}) {
         return {
             isServer,
+            articles: store.getState().articles,
+            addArticle,
         }
     }
     public render() {
         return (
             <Layout {...this.props}>
                 <h1>Article List</h1>
+                <button onClick={() => {
+                    this.props.addArticle()
+                }}>Add Article</button>
                 <ul>
-                    <li>
-                        <Link route="article" params={{articleId: 1}}>
-                            <a>
-                                <p>article 1 title</p>
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link route="article" params={{articleId: 2}}>
-                            <a>
-                                <p>article 2 title</p>
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link route="article/1">
-                            <a>
-                                <p>[fail-single] article 1 title</p>
-                            </a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link route="article/2">
-                            <a>
-                                <p>[fail-single] article 2 title</p>
-                            </a>
-                        </Link>
-                    </li>
+                    {this.props.articles.map((data) => {
+                        return <li>
+                            <Link route="article" params={{articleId: data.articleId}}>
+                                <a>
+                                    <p>article {data.articleId} title</p>
+                                </a>
+                            </Link>
+                            <Link route={`article/${data.articleId}`}>
+                                <a>
+                                    <p>[fail-single] article {data.articleId} title</p>
+                                </a>
+                            </Link>
+                        </li>
+                    })}
                 </ul>
             </Layout>
         )
     }
 }
 
-export default Index
+const mapStateToProps = (state, ownProps) => {
+    return {
+        articles: state.articles,
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addArticle: bindActionCreators(addArticle, dispatch),
+    }
+}
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Index)
