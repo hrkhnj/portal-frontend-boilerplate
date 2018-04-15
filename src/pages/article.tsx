9416c46b4e6557182f36ab7187aada023ab7592b
honjo hiorki <hrkhnj4eng@gmail.com>
2018-04-16 01:44:47 +0900

    page-article read store

--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -1,40 +1,27 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import withRedux from '../utils/withRedux'
+import { initStore } from '../store'
 import routes from '../routes'
 
 const { Link } = routes
 
-interface Query {
-    articleId: number
-}
-
-interface Url {
-    query: Query
-}
-
 interface ArticleProps {
     isServer: boolean,
-    url: Url;
+    articleId: number;
 }
 
 class Article extends React.Component<ArticleProps> {
-    public static async getInitialProps(args) {
-        const isServer = typeof window === 'undefined'
-        if (isServer) {
-            return {
-                isServer,
-                url: {
-                    args: args.query,
-                },
-            }
-        } else {
-            return {...args, isServer}
+    public static async getInitialProps({isServer, query}) {
+        return {
+            isServer,
+            articleId: query.articleId,
         }
     }
     public render() {
         return (
             <Layout {...this.props}>
-                <h1>Article Detail (articleId: { this.props.url.query.articleId })</h1>
+                <h1>Article Detail (articleId: { this.props.articleId })</h1>
                 <Link route="index">
                     <a>
                         <p>&lt;&lt; Back to Top</p>
@@ -45,9 +32,4 @@ class Article extends React.Component<ArticleProps> {
     }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//     }
-// }
-export default Article
-// export default withRedux(initStore, null, mapDispatchToProps)(Article)
+export default withRedux(initStore, null, null)(Article)
