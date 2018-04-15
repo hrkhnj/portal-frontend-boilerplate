b3a996bef0b9e506b7dea8a35d153690a433a39c
honjo hiorki <hrkhnj4eng@gmail.com>
2018-04-16 02:39:58 +0900

    articles as object, change from array

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -4,9 +4,16 @@ import { actionTypes } from '../actions/index'
 export default (state = initialStateDefault, action) => {
     switch (action.type) {
         case actionTypes.AddArticle:
+            const count = state.count + 1
             return Object.assign({}, state, {
-                count: state.count + 1,
-                articles: [...state.articles, {articleId: state.count + 1 }],
+                count,
+                articles: {
+                    ...state.articles,
+                    [count]: {
+                        id: state.count + 1,
+                        title: `article_${count}_added`,
+                    },
+                },
             })
         default: return state
     }
