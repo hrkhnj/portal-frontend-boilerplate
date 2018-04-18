bdbeadc47134492dcfd3c5d075b2fc723b0a0345
honjo hiorki <hrkhnj4eng@gmail.com>
2018-04-19 07:50:02 +0900

    rename type def

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withTypescript = require('@zeit/next-typescript')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = withTypescript({
     webpack(config, options) {
-        // if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+        if (options.isServer) {
+            config.plugins.push(new ForkTsCheckerWebpackPlugin())
+            // config.path.resolve(compiler.options.context, './tsconfig.json')
+        }
         return config
     },
 })
