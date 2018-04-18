28e91f94da57c40620c7047deebfd7ebd5fe0b6b
honjo hiorki <hrkhnj4eng@gmail.com>
2018-04-19 07:59:43 +0900

    enable server type-checking

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = withTypescript({
     webpack(config, options) {
         if (options.isServer) {
-            config.plugins.push(new ForkTsCheckerWebpackPlugin())
+            config.plugins.push(new ForkTsCheckerWebpackPlugin({
+                // tslint: true,
+                // tsconfig: path.resolve(config.context, '../tsconfig.json')
+            }))
             // config.path.resolve(compiler.options.context, './tsconfig.json')
         }
         return config
